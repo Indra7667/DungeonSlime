d5694672093736a22d1631d1660b8173017744f6
Indra7667 <82109260+Indra7667@users.noreply.github.com>
2026-01-05 11:18:04 +0700

    update common lib
    created core
    add dependence to common lib
    change solution file name

---
+++ CommonLib/Core.cs
@@ -0,0 +1,87 @@
+using System;
+using Microsoft.Xna.Framework; // add this first for extend Game
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+namespace CommonLib;
+
+public class Core : Game
+{
+    internal static Core s_instance;
+
+    /// <summary>
+    /// Reference to the Core instance
+    /// </summary>
+    public static Core Instance => s_instance;
+
+    /// <summary>
+    /// Gets the graphics device manager to control the presentation of graphics
+    /// </summary>
+    public static GraphicsDeviceManager Graphics {get; private set;}
+    
+    /// <summary>
+    /// Gets the graphics device used 
+    /// to create graphical resources and perform primitive rendering
+    /// </summary>
+    public static new GraphicsDevice GraphicsDevice {get; private set;}
+
+    /// <summary>
+    /// Spritebatch for all 2d rendering
+    /// </summary>
+    public static SpriteBatch SpriteBatch {get; private set;}
+
+    /// <summary>
+    /// Content manager to load global assets
+    /// </summary>
+    public static new ContentManager Content {get; private set;}
+
+    /// <summary>
+    /// Creates a new Core instance
+    /// </summary>
+    public Core(string title, int width, int height, bool fullScreen, bool isMouseVisible = true)
+    {
+        // ensure that multiple cores are not created
+        if(s_instance != null)
+        {
+            throw new InvalidOperationException($"Only a single core can be created");
+        }
+
+        // store reference to engine for global member access
+        s_instance = this;
+        
+        // create new graphic device manager
+        Graphics = new GraphicsDeviceManager(this);
+
+        // set the graphics defaults
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.PreferredBackBufferHeight = width;
+        Graphics.IsFullScreen = fullScreen;
+
+        // apply graphic presentation changes
+        Graphics.ApplyChanges();
+
+        // set window title
+        Window.Title = title;
+
+        // set the core's content manager 
+        // to a reference of the base Game's content manager
+        Content = base.Content;
+
+        // set root directory for contents
+        Content.RootDirectory = "Content";
+
+        // set mouse visibility
+        IsMouseVisible = isMouseVisible;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        // set the core's graphic device to a reference
+        // of the base Game's graphics device
+        GraphicsDevice = base.GraphicsDevice;
+
+        // create sprite batch
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+}

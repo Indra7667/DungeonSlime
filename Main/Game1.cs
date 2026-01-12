a9b67b68a052d43f3eee8808adae36a45905e624
Indra7667 <82109260+Indra7667@users.noreply.github.com>
2026-01-12 09:42:31 +0700

    importing logo
    currently using inline spritebatch on main game1.cs
    will separate it and put it on lib next

--- Main/Game1.cs
+++ Main/Game1.cs
@@ -7,9 +7,11 @@ namespace Main;
 
 public class Game1 : Core
 {
+    private Texture2D _logo;
+
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        
+
     }
 
     protected override void Initialize()
@@ -22,6 +24,7 @@ public class Game1 : Core
     protected override void LoadContent()
     {
         // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("assets/images/logo");
         base.LoadContent();
     }
 
@@ -37,9 +40,50 @@ public class Game1 : Core
 
     protected override void Draw(GameTime gameTime)
     {
+        // TODO: Add your drawing code here
+
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        Rectangle logoIcon = new(0, 0, 128, 128);
+        Rectangle logoWord = new(150, 34, 458, 58);
+
+        SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
+        // icon
+        SpriteBatch.Draw(
+            _logo, // texture2D
+            new Vector2( // position, middle of the screen
+                Window.ClientBounds.Width,
+                Window.ClientBounds.Height
+            ) * 0.5f,
+            logoIcon,   // rectangle
+            Color.White * 1.0f, // color masking & opacity
+            MathHelper.ToRadians(0),   // rotation
+            new Vector2( // origin, middle of original image
+                logoIcon.Width,
+                logoIcon.Height
+            ) * 0.5f, 
+            1.0f,   // scale -> float | vector2d(x, y)
+            SpriteEffects.None, // effects -> None, FlipHorizontally, FlipVertically
+            1.0f    // layerDepth
+            );
+        SpriteBatch.Draw(
+            _logo, // texture2D
+            new Vector2( // position, middle of the screen
+                Window.ClientBounds.Width,
+                Window.ClientBounds.Height
+            ) * 0.5f,
+            logoWord,   // rectangle
+            Color.White * 1.0f, // color masking & opacity
+            MathHelper.ToRadians(0),   // rotation
+            new Vector2( // origin, middle of original image
+                logoWord.Width,
+                logoWord.Height
+            ) * 0.5f, 
+            1.0f,   // scale -> float | vector2d(x, y)
+            SpriteEffects.None, // effects -> None, FlipHorizontally, FlipVertically
+            0.0f    // layerDepth
+            );
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }

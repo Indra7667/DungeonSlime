f3ae28497f2a90a707dd0184c49b29878c29511f
Indra7667 <82109260+Indra7667@users.noreply.github.com>
2026-01-12 10:32:21 +0700

    add texture handler to lib

---
+++ CommonLib/Graphics/TextureRegion.cs
@@ -0,0 +1,132 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CommonLib.Graphics;
+
+public class TextureRegion
+{
+    // attributes
+    /// <summary>
+    /// get or set the source texture 
+    /// of this region
+    /// </summary>
+    public Texture2D Texture { get; set; }
+
+    /// <summary>
+    /// get or set the rectangle boundary 
+    /// of this region
+    /// within the source
+    /// </summary>
+    public Rectangle SourceRectangle { get; set; }
+
+    /// <summary>
+    /// get the width 
+    /// of this texture region
+    /// </summary>
+    public int Width => SourceRectangle.Width;
+
+    /// <summary>
+    /// get the height
+    /// of this texture region
+    /// </summary>
+    public int Height => SourceRectangle.Height;
+
+    // construct
+    /// <summary>
+    /// create new empty instance 
+    /// of this class
+    /// </summary>
+    public TextureRegion() { }
+
+    /// <summary>
+    /// create new filled instance 
+    /// of this class
+    /// using specified source texture2D
+    /// </summary>
+    /// <param name="texture">texture2d as source of this texture region</param>
+    /// <param name="x">x coord position of the upper left corner of this texture region</param>
+    /// <param name="y">y coord poition of the upper left corner of this texture region</param>
+    /// <param name="width">the width of this texture region</param>
+    /// <param name="height">the height of this texture region</param>
+    public TextureRegion(Texture2D texture, int x, int y, int width, int height)
+    {
+        Texture = texture;
+        SourceRectangle = new Rectangle(x, y, width, height);
+    }
+
+
+    // methods
+    /// <summary>
+    /// Submit this texture region for drawing in the current batch.
+    /// have overflow(s)
+    /// </summary>
+    /// <param name="spriteBatch">The spritebatch instance used for batching draw calls.</param>
+    /// <param name="position">The xy-coordinate location to draw this texture region on the screen.</param>
+    /// <param name="color">The color mask to apply when drawing this texture region on screen.</param>
+    public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color)
+    {
+        Draw(
+            spriteBatch,
+            position,
+            color,
+            0.0f,
+            Vector2.Zero,
+            Vector2.One,
+            SpriteEffects.None,
+            0.0f
+        );
+    }
+
+    /// <summary>
+    /// Submit this texture region for drawing in the current batch.
+    /// have overflow(s)
+    /// </summary>
+    /// <param name="spriteBatch">The spritebatch instance used for batching draw calls.</param>
+    /// <param name="position">The xy-coordinate location to draw this texture region on the screen.</param>
+    /// <param name="color">The color mask to apply when drawing this texture region on screen.</param>
+    /// <param name="rotation">The amount of rotation, in radians, to apply when drawing this texture region on screen.</param>
+    /// <param name="origin">The center of rotation, scaling, and position when drawing this texture region on screen.</param>
+    /// <param name="scale">The scale factor to apply when drawing this texture region on screen.</param>
+    /// <param name="effects">Specifies if this texture region should be flipped horizontally, vertically, or both when drawing on screen.</param>
+    /// <param name="layerDepth">The depth of the layer to use when drawing this texture region on screen.</param>
+    public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)
+    {
+        Draw(
+            spriteBatch,
+            position,
+            color,
+            rotation,
+            origin,
+            new Vector2(scale, scale),
+            effects,
+            layerDepth
+        );
+    }
+
+    /// <summary>
+    /// Submit this texture region for drawing in the current batch.
+    /// overflowed
+    /// </summary>
+    /// <param name="spriteBatch">The spritebatch instance used for batching draw calls.</param>
+    /// <param name="position">The xy-coordinate location to draw this texture region on the screen.</param>
+    /// <param name="color">The color mask to apply when drawing this texture region on screen.</param>
+    /// <param name="rotation">The amount of rotation, in radians, to apply when drawing this texture region on screen.</param>
+    /// <param name="origin">The center of rotation, scaling, and position when drawing this texture region on screen.</param>
+    /// <param name="scale">The amount of scaling to apply to the x- and y-axes when drawing this texture region on screen.</param>
+    /// <param name="effects">Specifies if this texture region should be flipped horizontally, vertically, or both when drawing on screen.</param>
+    /// <param name="layerDepth">The depth of the layer to use when drawing this texture region on screen.</param>
+    public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effects, float layerDepth)
+    {
+        spriteBatch.Draw(
+            Texture,
+            position,
+            SourceRectangle,
+            color,
+            rotation,
+            origin,
+            scale,
+            effects,
+            layerDepth
+        );
+    }
+}
